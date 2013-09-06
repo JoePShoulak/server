65381513914184c4de8ec0bc34c23786b83ae8c5
Joe Shoulak <JoepShoulak@me.com>
2013-09-05 23:10:13 -0500

    Error handling

--- client.rb
+++ client.rb
@@ -1,11 +1,50 @@
 require 'socket'
 
-a = TCPSocket.new('127.0.0.1', 3333)
-puts "> Connection made to server"
-puts "> What would you like to say?"
-print "> "
-a.write gets.chomp
-puts "> Sending..."
-puts "> Received: #{a.recv(1024)}"
-a.close
-puts "> Disconnected from server"
+class Session
+  attr_accessor :socket
+
+  def connect
+    self.socket = TCPSocket.new('127.0.0.1', 3333)
+  end
+
+  def send(message)
+    self.socket.write(message)
+  end
+
+  def get
+    self.socket.recv(1024)
+  end
+
+  def close
+    self.socket.close
+  end
+end
+
+
+s = Session.new
+s.connect
+if s.socket != nil
+  begin
+    puts "> Connection made to server"
+    message = ""
+    while message.length.zero?
+      puts "> What would you like to say?"
+      print "> "
+      message = gets.chomp
+      if message.length.zero?
+        puts "> Nothing entered"
+      end
+    end
+    s.send(message) 
+    puts "> Sending..."
+    puts "> Received: #{s.get}"
+    s.close
+    puts "> Disconnected from server"
+  rescue Interrupt
+    puts "\n> User canceled"
+    s.close
+    puts "> Disconnected from server"
+  end
+else
+  puts "> Server connection error"
+end
--- server.rb
+++ server.rb
@@ -7,11 +7,16 @@ begin
     connection = a.accept
     puts "> Connection made"
     message = connection.recv(1024)
-    puts "  > received: #{message}"
-    puts "  > sending:  #{message.reverse}"
-    connection.write(message.reverse)
-    connection.close
-    puts "  > connection closed"
+    if message.length.zero?
+      puts "> User disconnected"
+    else
+      puts "  > received: #{message}"
+      puts "  > #{message.class}"
+      puts "  > sending:  #{message.reverse}"
+      connection.write(message.reverse)
+      connection.close
+      puts "  > connection closed"
+    end
   }
 rescue Interrupt => e
   puts "\n> Server halted"
