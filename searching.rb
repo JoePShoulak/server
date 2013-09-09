275754ef001226fd7f767d2d2350ecd37c8c62ea
Joe Shoulak <JoepShoulak@me.com>
2013-09-09 09:54:02 -0500

    working on efficiency

--- searching.rb
+++ searching.rb
@@ -7,18 +7,18 @@ def find_center(l) # Returns the greatest power of 2 beneath the length of l
 end
 
 def binary_search(list, item) # Finds the item in O(log n)
-  it = find_center(list) # The largest power of 2 beneath list's length
-  place = it # This is where we start
+  place = list.length / 2 # The largest power of 2 beneath list's length
+  it = 1 # This is where we start
   found = false # We haven't found it yet
-  while it != 0 && !found # While we haven't found it and there are still places to look...
-    it /= 2
+  while 2**it < list.length && !found # While we haven't found it and there are still places to look...
     if list[place] == item # If where we are is what we're searching for, we found it
       found = true
     elsif list[place] < item # Too far? Go back.
-      place += it
+      place += list.length / it
     elsif list[place] > item # Too early? Go forward.
-      place -= it
+      place -= list.length / it
     end
+    it *= 2
   end
   if found # If we found it...
     place # Return the position of it in the list
